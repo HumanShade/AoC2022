22805ce8f973ac513fe96e2cd27e3601a2001c6c
Norman Janurberg <njanurberg@gmail.com>
2022-12-10 14:52:04 +0100

    day 6 complete

---
+++ day6/problem2.py
@@ -0,0 +1,18 @@
+# file_name = 'example1.txt'
+# file_name = 'example2.txt'
+# file_name = 'example3.txt'
+# file_name = 'example4.txt'
+# file_name = 'example5.txt'
+# file_name = 'example2.txt'
+file_name = 'input.txt'
+
+def main():
+    with open(file_name, 'r') as f:
+        line = f.read()
+        for idx in range(len(line)):
+            if len(set(line[idx:idx+14])) == 14:
+                print(f"Marker after: {idx+14}")
+                break
+
+if __name__ == "__main__":
+    main()
